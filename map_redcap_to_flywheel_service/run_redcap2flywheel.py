767e491c06f62d3c03a41ecbf486d4f1cfea64fd
David Parker <davidparker@flywheel.io>
2022-06-23 15:14:30 -0400

    reviewer changes to run_redcap2flywheel.py

--- map_redcap_to_flywheel_service/run_redcap2flywheel.py
+++ map_redcap_to_flywheel_service/run_redcap2flywheel.py
@@ -7,17 +7,17 @@ import argparse as ap
 parser = ap.ArgumentParser(
     description="Uploads metadata from a redcap instance to a flywheel project")
 
-parser.add_argument('-f', '--fwapi', type=str,
-                    help="The API key for the Flywheel instance that you are uploading data to")
-
-parser.add_argument('-p', '--projectID', type=str,
-                    help="The ID of the Flywheel project you are uploading data to.")
-
 parser.add_argument('-r', '--rcAPI', type=str,
-                    help="The API key for the REDCap instance that you are downloading data from.")
+                    help="The API key for the source REDCap instance that you are downloading data from.")
 
 parser.add_argument('-u', '--rcURL', type=str,
-                    help="The URL of the REDCap instance that you are downloading data from.")
+                    help="The URL of the source REDCap instance that you are downloading data from.")
+
+parser.add_argument('-f', '--fwapi', type=str,
+                    help="The API key for the target Flywheel instance that you are uploading data to")
+
+parser.add_argument('-p', '--projectID', type=str,
+                    help="The ID of the target Flywheel project you are uploading data to.")
 
 parser.add_argument('-y', '--yamlFile', type=str,
                     help="The full path to the yaml file that describes what metadata to download")
@@ -25,21 +25,21 @@ parser.add_argument('-y', '--yamlFile', type=str,
 
 def get_missing_inputs(args):
     if args['fwapi'] is None:
-        args['fwapi'] = getpass.getpass(prompt="Please enter your flylwheel API key")
+        args['fwapi'] = getpass.getpass(prompt="Please enter your API key of the target Flywheel instance")
 
     if args['projectID'] is None:
         args['projectID'] = getpass.getpass(
-            prompt='Please enter the ID of the Project to extract data from')
+            prompt='Please enter the ID of the target project in Flywheel')
 
     if args['rcURL'] is None:
         args['rcURL'] = getpass.getpass(
-            prompt='Please enter the URL of the REDCap project to upload data to')
+            prompt='Please enter the URL of the source REDCap instance')
 
     if args['rcAPI'] is None:
-        args['rcAPI'] = getpass.getpass(prompt='Please enter your REDCap API token')
+        args['rcAPI'] = getpass.getpass(prompt='Please enter your REDCap API token for the source REDCap instance')
 
     if args['yamlFile'] is None:
-        args['yamlFile'] = input('Please enter the location of the flywheel to redcap yaml file')
+        args['yamlFile'] = input('Please enter the location of the REDCap to Flywheel .yaml file')
 
     return (args)
 
@@ -47,11 +47,16 @@ def get_missing_inputs(args):
 ###################################
 
 
-def main(fwapi, projectID, yamlFile, rcAPI, rcURL):
+def main(fwapi, fwprojectID, yamlFile, rcAPI, rcURL):
 
-    # Initialize the fw client and redcap project
+    # Initialize api connection to flywheel instance and redcap project.
+    # A flywheel API key allows you to access anything on the flywheel instance (site)
+    # that your loging has permissions to access.
+    # A redcap API token allows you ONLY access to one specific project, which is a collection
+    # of records.
+
     fw = flywheel.Client(fwapi)
-    project = fw.get_project(projectID)
+    project = fw.get_project(fwprojectID)
 
     # Get the dictionary of redcap metadata to upload to flywheel
     rc_dict, fw_key_level, fw_key = yd.generate_redcap_dict(yamlFile, rcAPI, rcURL)
@@ -68,4 +73,4 @@ if __name__ == '__main__':
 
 # TODO:
 # - create subjects if ID doesn't exist
-# - create sessions off of
+# - create sessions based off forms or records or something?
